41c0ed795a6373a625255fe9fe0053852e30a6d1
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-04-05 15:45:53 -0600

    add sad path for application_creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-<<<<<<< Updated upstream
-
-=======
-# PetApplication.destroy_all
-# Pet.destroy_all
-# Application.destroy_all
-# Shelter.destroy_all
->>>>>>> Stashed changes
-
 
 @shelter_1 = Shelter.create!(foster_program: true, name: "Taj Mahal for Dogs", city: "Sky City", rank: 20)
 @shelter_2 = Shelter.create!(foster_program: true, name: "Valhalla for Cats", city: "Sky City", rank: 30)
